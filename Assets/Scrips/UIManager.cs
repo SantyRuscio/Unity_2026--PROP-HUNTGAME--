ad1de098512308b0ed75143e2282aa356f94f5e2
SantyRuscio <santiruscio1237@gmail.com>
2026-05-02 16:05:02 -0300

    Contador Para el Hunter

--- Assets/Scrips/UIManager.cs
+++ Assets/Scrips/UIManager.cs
@@ -9,10 +9,66 @@ public class UIManager : MonoBehaviour
     public GameObject gameOverPanel;
     public TextMeshProUGUI winnerText;
 
+    [Header("Hide Timer UI")]
+    public TextMeshProUGUI hideTimerText;
+
     private void Awake()
     {
         Instance = this;
         gameOverPanel.SetActive(false);
+
+        if (hideTimerText != null)
+            hideTimerText.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (hideTimerText == null) return;
+
+        Player hunter = BuscarHunter();
+
+        if (hunter == null)
+        {
+            hideTimerText.gameObject.SetActive(false);
+            return;
+        }
+
+        if (hunter.HideTimer.IsRunning)
+        {
+            float tiempo = hunter.HideTimer.RemainingTime(hunter.Runner) ?? 0f;
+            int segundos = Mathf.CeilToInt(tiempo);
+
+            hideTimerText.text = "Tiempo De Escondite: " + segundos;
+
+            if (segundos <= 3)
+            {
+                hideTimerText.color = Color.red;
+            }
+            else
+            {
+                hideTimerText.color = Color.white;
+            }
+
+            hideTimerText.gameObject.SetActive(true);
+        }
+        else
+        {
+            hideTimerText.gameObject.SetActive(false);
+        }
+    }
+
+
+    Player BuscarHunter()
+    {
+        var players = FindObjectsByType<Player>(FindObjectsSortMode.None);
+
+        foreach (var p in players)
+        {
+            if (p.isHunter)
+                return p;
+        }
+
+        return null;
     }
 
     public void ShowGameOver(string message)
@@ -28,7 +84,6 @@ public class UIManager : MonoBehaviour
     {
         TriggerRestart();
     }
-
 
     private void TriggerRestart()
     {
